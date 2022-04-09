fdc06d7fa745ecb3838ac84434e9344f0ffddf15
d-herbert <deidre.m.herbert@gmail.com>
2022-04-09 16:23:30 -0700

    formatting changes, delete button works

--- js/script.js
+++ js/script.js
@@ -15,30 +15,31 @@ form.addEventListener('submit', (e) => {
     let extension = document.querySelector('#extension').value 
     let email = document.querySelector('#email').value 
     let department = document.querySelector('#department').value
+
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
-    let row = document.createElement('tr')
+    let row = table.insertRow()
+
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
-    let cell0 = row.appendChild(document.createElement('td'))
-    let cell1 = row.appendChild(document.createElement('td'))
-    let cell2 = row.appendChild(document.createElement('td'))
-    let cell3 = row.appendChild(document.createElement('td'))
-    let cell4 = row.appendChild(document.createElement('td'))
-    let item0 = document.createTextNode(id)
-    let item1 = document.createTextNode(name)
-    let item2 = document.createTextNode(extension)
-    let item3 = document.createTextNode(email)
-    let item4 = document.createTextNode(department)
+    let cellID1 = row.insertCell()
+    let cellID2 = row.insertCell()
+    let cellID3 = row.insertCell()
+    let cellID4 = row.insertCell()
+    let cellID5 = row.insertCell()
+    let cell_delete = row.insertCell()
+
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
-    cell0.appendChild(item0)
-    cell1.appendChild(item1)
-    cell2.appendChild(item2)
-    cell3.appendChild(item3)
-    cell4.appendChild(item4)
+    cellID1.appendChild(document.createTextNode(id))
+    cellID2.appendChild(document.createTextNode(name))
+    cellID3.appendChild(document.createTextNode(extension))
+    cellID4.appendChild(document.createTextNode(email))
+    cellID5.appendChild(document.createTextNode(department))
+
      // CREATE THE DELETE BUTTON
     let deleteBtn = document.createElement('button')
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
-    let textDelete = document.createTextNode('X')
-    deleteBtn.appendChild(textDelete)
+    deleteBtn.appendChild(document.createTextNode('X'))
+    cell_delete.appendChild(deleteBtn)
+
     // RESET THE FORM
     document.querySelector('#id').value = ''
     document.querySelector('#name').value = ''
@@ -47,15 +48,18 @@ form.addEventListener('submit', (e) => {
     document.querySelector('#department').value = ''
     // SET FOCUS BACK TO THE ID TEXT BOX
     table.appendChild(row)
-});
-
-row.addEventListener('click', (e) => {
+    document.getElementById('id').focus();
     // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
-    if (e.target.classList.contains('delete')) {
-        if (confirm(`are you sure you want to delete? ${e.target.parentElement.innerText}`)) {
-            // select 
-            row.removeChild(e.target.parentElement)
-        }
-    }
+    count+= 1
+    console.log(count)
 })
-// DELETE EMPLOYEE
+
+table.addEventListener('click', (e) => {
+    var i = e.target.parentNode.parentNode.rowIndex
+    if (confirm(`are you sure you want to delete?`)) {
+        // DELETE EMPLOYEE
+        table.deleteRow(i)
+        count-=1
+    }
+    console.log(count)
+})
