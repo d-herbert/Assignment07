0df70fa59607ca5a543ddc7f8754a8439bf3cd60
d-herbert <deidre.m.herbert@gmail.com>
2022-04-09 14:28:31 -0700

    table appending is working

--- js/script.js
+++ js/script.js
@@ -10,27 +10,37 @@ form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
     e.preventDefault()
     // GET THE VALUES FROM THE TEXT BOXES
+    let id = document.querySelector('#id').value 
     let name = document.querySelector('#name').value 
     let extension = document.querySelector('#extension').value 
     let email = document.querySelector('#email').value 
     let department = document.querySelector('#department').value
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
-    let row = document.createElement('li')
-    row.className = 'list-group-item'
+    let row = document.createElement('tr')
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    let cell0 = row.appendChild(document.createElement('td'))
+    let cell1 = row.appendChild(document.createElement('td'))
+    let cell2 = row.appendChild(document.createElement('td'))
+    let cell3 = row.appendChild(document.createElement('td'))
+    let cell4 = row.appendChild(document.createElement('td'))
+    let item0 = document.createTextNode(id)
     let item1 = document.createTextNode(name)
     let item2 = document.createTextNode(extension)
     let item3 = document.createTextNode(email)
     let item4 = document.createTextNode(department)
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
-    row.appendChild(item1, item2, item3, item4)
-    
-    // CREATE THE DELETE BUTTON
+    cell0.appendChild(item0)
+    cell1.appendChild(item1)
+    cell2.appendChild(item2)
+    cell3.appendChild(item3)
+    cell4.appendChild(item4)
+     // CREATE THE DELETE BUTTON
     let deleteBtn = document.createElement('button')
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
     let textDelete = document.createTextNode('X')
     deleteBtn.appendChild(textDelete)
     // RESET THE FORM
+    document.querySelector('#id').value = ''
     document.querySelector('#name').value = ''
     document.querySelector('#extension').value = ''
     document.querySelector('#email').value = ''
